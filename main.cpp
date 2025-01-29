fd1ebccfa61c4bfeecfa15bae7d48e5411f7d6fd
Baril1 <omar.hamdi204@gmail.com>
2025-01-29 16:34:21 +0100

    fixed play button

--- main.cpp
+++ main.cpp
@@ -15,38 +15,41 @@ int main() {
         std::cerr << "Error loading font!" << std::endl;
         return -1;
     }
-    RectButton button(buttonFont, sf::Vector2f(250.f, 70.f), sf::Vector2f(100.f, 350.f));
+
+    RectButton button(buttonFont, sf::Vector2f(250.f, 70.f), sf::Vector2f(50.f, 250.f));
     button.setButtonLabel(40.f, "Play");
 
-    bool displayNewWindow = false; 
+    bool isMenuActive = true;  // Track if we are on the main menu
 
-    menu.displayAnimation(window); 
+    menu.displayAnimation(window);
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
-                return 0; 
+                return 0;
             }
+
             button.getButtonStatus(window, event);
 
+            // Switch to another "page" when the button is clicked
             if (button.isPressed) {
-                std::cout << "Button clicked!" << std::endl;
-                displayNewWindow = true; 
+                isMenuActive = false;  // Move to the next page
             }
         }
 
         window.clear();
-        
-        if (displayNewWindow) {
-            menu.display(window, "../assets/ala.jpg"); 
+
+        if (isMenuActive) {
+            menu.display(window, "../assets/Group 2.png");
+            button.draw(window);
         } else {
-            menu.display(window, "../assets/Group 2.png"); 
+            menu.display(window, "../assets/ala.jpg");  // Display new "page"
         }
-        button.draw(window);
+
         window.display();
     }
 
-    return 0; 
+    return 0;
 }
